de40a333771eff925ff3a702f4213b93753c3134
MechlerLucas <mechlerlucas@outlook.com>
2022-11-26 15:25:13 -0300

    Update grafos.py

--- grafos.py
+++ grafos.py
@@ -7,24 +7,24 @@ import networkx as nx
 
 def Matrix():
     reader = csv.reader(open("grafo.csv"), delimiter=";")
-    return numpy.array(list(reader))
+    matrizAdj = numpy.array(list(reader))
 
-matrizAdj = Matrix()
+    print(matrizAdj)
 
-print(matrizAdj)
+    grafo = nx.DiGraph()
 
-grafo = nx.DiGraph()
-
-# Preenche os vertices do grafo a partir da primeira coluna da matriz de adjacencia
-for k in range(1, len(matrizAdj)):
-    grafo.add_node(matrizAdj[k][0])
+    # Preenche os vertices do grafo a partir da primeira coluna da matriz de adjacencia
+    for k in range(1, len(matrizAdj)):
+        grafo.add_node(matrizAdj[k][0])
 
 
-# Preenche as arestas do grafo a partir da matriz de adjacencia
-for i in range(0, len(matrizAdj)):
-    for j in range(0, len(matrizAdj)):
-        if matrizAdj[i][j] == '1':
-            print(i, j)
-            grafo.add_edge(matrizAdj[i][0], matrizAdj[0][j])
-print(grafo.adj)
+    # Preenche as arestas do grafo a partir da matriz de adjacencia
+    for i in range(0, len(matrizAdj)):
+        for j in range(0, len(matrizAdj)):
+            if matrizAdj[i][j] == '1':
+                print(i, j)
+                grafo.add_edge(matrizAdj[i][0], matrizAdj[0][j])
+    print(grafo.adj)
+
+    return grafo
 
