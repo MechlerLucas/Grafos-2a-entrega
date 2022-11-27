05e157df781531168b9061241892cd9c33705d7e
MechlerLucas <mechlerlucas@outlook.com>
2022-11-27 14:52:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import networkx as nx
 import numpy as np
 import pandas as pd
+import grafosArq
 
 def Naive(grafo):
+
+    list (nx.bridges(grafos))
+    #for i in grafo.number_of_nodes():
 
 
     return None
@@ -11,3 +15,6 @@ def Tarjan(grafo):
 
 
     return None
+
+
+Naive(EntMatriz())
