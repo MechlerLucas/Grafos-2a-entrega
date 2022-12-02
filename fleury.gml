5fb394b6dec7bc5e7381a31befafbb0812264d85
MechlerLucas <mechlerlucas@outlook.com>
2022-12-02 18:08:56 -0300

    Alteração de fleury

---
+++ fleury.gml
@@ -0,0 +1,33 @@
+Creator "igraph version 0.10.2 Tue Nov 29 16:35:05 2022"
+Version 1
+graph
+[
+  directed 0
+  node
+  [
+    id 0
+  ]
+  node
+  [
+    id 1
+  ]
+  node
+  [
+    id 2
+  ]
+  edge
+  [
+    source 0
+    target 1
+  ]
+  edge
+  [
+    source 1
+    target 2
+  ]
+  edge
+  [
+    source 2
+    target 0
+  ]
+]
--- metodos.py
+++ metodos.py
@@ -12,11 +12,12 @@ def Info(grafo):
     print("Pontes:        ",grafo.bridges())
     print("Componentes:   ",len(grafo.clusters()))
     print("Lista vertices:",grafo.vs.indices)
+
     listaArestas = []
     for e in grafo.es:
         listaArestas.append(e.tuple)
     print("---------------------------")
-    print(*listaArestas, sep = "\n")
+    print("Arestas:", *listaArestas, sep = "\n")
     print("---------------------------")
 
 #Metodo de Naive para aresta separada
@@ -53,37 +54,45 @@ def Tarjan(grafo):
 #Metodo de Fleury
 def Fleury(grafo):
     Info(grafo)
+    print("Informações da função de Fleury")
     listaAdjacentes = grafo.get_adjlist(mode = "all")
-    print(*listaAdjacentes, sep = "\n")
+    print(*listaAdjacentes, sep = ", ")
     graus = grafo.degree()
-    print(graus)
+    print("Graus: ",graus)
+    print("---------------------------")
     for i in graus:
         if graus[i] % 2 or graus[i] == 1:
             print("Grafo sem caminho euleriano")
             quit()
-    
+    print("Grafo euleriano")
+
     listaVertices = []
     for e in grafo.vs:
-        listaVertices.append(e.tuple)
+        listaVertices.append(e)
+    print("Vertices ",*listaVertices, sep = ", ")
 
     listaArestas = []
     for e in grafo.es:
         listaArestas.append(e.tuple)
+    print("Arestas ",*listaArestas, sep = ", ")
 
     #--------------------------------------------------#
     i = 0
     Tour = []
 
     aux = grafo.copy()
-    v0 = listaVertices[0]
-    Tour.append[v0]
+    v0 = listaVertices(0)
+    Tour.append(v0)
     
-    while not bool(listaArestas):
-        vi = Tour[i]
+    while bool(listaArestas):
+
+        print(i, " Tour ", *Tour, sep=",")
+        indice = len(Tour) - 1
+        vi = Tour(indice)
 
         if len(aux.neighbors(vi)) > 1:
             for j in aux.neighbors(vi):
-                if NaiveAresta(aux, (vi, j)):
+                if not NaiveAresta(aux, (vi, j)):
                     arestaRetirada = (vi, j)
                     J = j
                     break
@@ -96,6 +105,7 @@ def Fleury(grafo):
         vj = J
         Tour.append(vj)
 
+        i += 1
         aresta = "("+vi+", "+vj+")"
         listaArestas.remove(aresta)
     return Tour
@@ -107,9 +117,13 @@ def Aleatorio(nodos):
     grafoRand = ig.Graph.Erdos_Renyi(n=nodos, p = 0.2, directed=False, loops=True)
     return grafoRand
 
-Info(ImportaArq('entrada.gml'))
 
-NaiveAresta(ImportaArq('entrada.gml'),(4, 5))
+print("Tour", *Fleury(ImportaArq('fleury.gml')), sep = ", ")
+
+
+#Info(ImportaArq('entrada.gml'))
+
+#NaiveAresta(ImportaArq('entrada.gml'),(4, 5))
 
 #Fleury(ImportaArq('entrada.gml'))
 #Naive(ImportaArq('entrada.gml'))
