5432b83fcf1be4eda264ddbf294829c447d771f9
MechlerLucas <mechlerlucas@outlook.com>
2022-12-03 01:53:03 -0300

    Remoção de PRINT() desnecessarios

--- metodos.py
+++ metodos.py
@@ -25,24 +25,13 @@ def Info(grafo):
     print("------------Infos------------\n\n")
 #Metodo de Naive para aresta separada
 def NaiveAresta(grafo, aresta):
-    print("\n\n------------Naive------------")
     aux = grafo.copy()
-    print("Deletando: ",aresta)
     aux.delete_edges(aresta)
 
-    print("Qtd nova:    ",len(aux.clusters()))
-    print("Qtd inicial: ",len(grafo.clusters()))
-
     if (len(aux.clusters()) > len(grafo.clusters())):
-        print(aresta, "é uma ponte")
-        print("------------Naive------------")
         return True
     else:
-        print(aresta, " não é uma ponte")
-        print("------------Naive------------\n\n")
         return False
-
-
 
 #Metodo de Tarjan
 def Tarjan(grafo):
@@ -56,8 +45,7 @@ def Tarjan(grafo):
 
 #Metodo de Fleury
 def Fleury(grafo):
-    #Info(grafo)
-    print("\n\n------------Fleury-----------")
+    Info(grafo)
     #--------------------------------------------------#
     #Verifica se o grafo é eulericano
     graus = grafo.degree()
@@ -81,20 +69,12 @@ def Fleury(grafo):
     Tour.append(v0)
     vj = v0
     while bool(listaArestas):
-
-        print("Loop ",i)
-        print("Tour =",*Tour, sep="")
         
         vi = vj
         J = 0
-
-        print("vi =",vi )
-        print("Vertice selecionado: ", vi)
         qtdn = aux.neighbors(vi,mode='all')
-        print("Quantidade neib = ",len(qtdn))
 
         if  len(qtdn) > 1:
-            print("\n\nAdjacentes:", *aux.neighbors(vi, mode='all'), sep=';')
             for k in qtdn:
 
                 if not NaiveAresta(aux, (vi, k)):
@@ -106,23 +86,17 @@ def Fleury(grafo):
             J = qtdn[0]
             arestaRetirada = (vi, J)
 
-        print("Deletando: ",arestaRetirada)
-
         aux.delete_edges(arestaRetirada)
         
         listaArestasi = []
         for e in aux.es:
             listaArestasi.append(e.tuple)
-        print("---------------------------")
-        print("Arestas:", *listaArestasi, sep = "; ")
         Tour.append(arestaRetirada)
         vj = J
         Tour.append(vj)
-        print("VJ = ",vj,"\n---")
 
         listaArestas.pop(i)
         i -= 1
-    print("------------Fleury-----------\n\n")
     return Tour
 
 #Criador aleatorio de grafos
@@ -131,14 +105,4 @@ def Aleatorio(nodos):
     grafoRand = ig.Graph.Erdos_Renyi(n=nodos, p = 0.2, directed=False, loops=True)
     return grafoRand
 
-
-print("Tour de saida", *Fleury(ImportaArq('fleury.gml')), sep = ", ")
-
-
-#Info(ImportaArq('entrada.gml'))
-
-#NaiveAresta(ImportaArq('entrada.gml'),(4, 5))
-
-#Fleury(ImportaArq('entrada.gml'))
-#Naive(ImportaArq('entrada.gml'))
-#Info(Aleatorio(1000))
+print("Tour Euleriano", *Fleury(ImportaArq('fleury.gml')), sep = ", ")
